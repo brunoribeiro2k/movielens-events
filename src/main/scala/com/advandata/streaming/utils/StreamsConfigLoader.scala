37aa9613325cdef4a5e07609a3634e9f093a2711
Bruno Ribeiro <fett2k@gmail.com>
2023-09-27 23:06:37 +0100

    Set commit interval to 1 second

--- src/main/scala/com/advandata/streaming/utils/StreamsConfigLoader.scala
+++ src/main/scala/com/advandata/streaming/utils/StreamsConfigLoader.scala
@@ -11,10 +11,11 @@ object StreamsConfigLoader {
   final val SCHEMA_REGISTRY_URL_CONFIG = "schema.registry.url"
   def load(config: Config): Properties = {
     val propMap = Map(
+      SCHEMA_REGISTRY_URL_CONFIG -> config.getString("kafka.schema-registry.url"),
       StreamsConfig.APPLICATION_ID_CONFIG -> config.getString("app.name"),
       StreamsConfig.BOOTSTRAP_SERVERS_CONFIG -> config.getString("kafka.bootstrap-servers.url"),
       ConsumerConfig.AUTO_OFFSET_RESET_CONFIG -> config.getString("kafka.offset-reset"),
-      SCHEMA_REGISTRY_URL_CONFIG -> config.getString("kafka.schema-registry.url")
+      StreamsConfig.COMMIT_INTERVAL_MS_CONFIG -> config.getString("kafka.commit-interval-ms"),
     )
     val props = new Properties()
     propMap.foreach({ case (k, v) => props.put(k, v)})
